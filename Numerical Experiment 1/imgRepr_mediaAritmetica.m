0086b88cd48bf76ba9a17aa66a9e2ba0cbefbbb2
jusotoTEC <102713314+jusotoTEC@users.noreply.github.com>
2023-07-31 13:57:09 -0600

    Update imgRepr_mediaAritmetica.m

--- Numerical Experiment 1/imgRepr_mediaAritmetica.m
+++ Numerical Experiment 1/imgRepr_mediaAritmetica.m
@@ -1,9 +1,7 @@
 function [imgRepr,m,n]=imgRepr_mediaAritmetica(direccion,nombre,extension)
     
-    
     a=dir([direccion '\' '/*.' extension]);
     numImagenes=size(a,1);
-
     [m,n,s]=size(im2double(imread([direccion '\' nombre ' (1).' extension])));
     imgRepr=zeros(m,n,s);
     for k=1:s
@@ -13,9 +11,7 @@ function [imgRepr,m,n]=imgRepr_mediaAritmetica(direccion,nombre,extension)
             Aux=im2double(imread(text));
             B(:,:,i)=Aux(:,:,k);   
         end
-
         imgRepr(:,:,k)=mean(B,3);   
     end
-    
     imgRepr=im2uint8(imgRepr);
 end
